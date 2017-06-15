3449a701822eb7c922b13d3207866d5d3b598039
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-06-15 11:04:43 -0400

    latest

--- HosoKeys.py
+++ HosoKeys.py
@@ -8,8 +8,13 @@ from tkinter import *
 import tkinter
 
 kb = tkinter.Tk()
-kb.title("HosoKeys")
-kb.resizable(0,0)
+
+buttons = [
+'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','\\','7','8','9','BACK',
+'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','[',']','4','5','6'
+,'SHIFT',
+'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.','?','/','1','2','3','SPACE',
+]
 
 def select(value):
 	if value == "BACK":
@@ -24,42 +29,53 @@ def select(value):
 	else :
 		entry.insert(tkinter.END,value)
 
-buttons = [
-'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','\\','7','8','9','BACK',
-'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','[',']','4','5','6'
-,'SHIFT',
-'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.','?','/','1','2','3','SPACE',
-]
 
-label1 = Label(kb,text='               ').grid(row=0,columnspan=15)
-entry = Entry(kb,width=50)
-entry.grid(row=1,columnspan=15)
 
-varRow = 2
-varColumn = 0
+def HosoPop():
 
-for button in buttons:
+	varRow = 2
+	varColumn = 0
 
-	command = lambda x=button: select(x)
+	for button in buttons:
+
+		command = lambda x=button: select(x)
+		
+		if button == "SPACE" or button == "SHIFT" or button == "BACK":
+			tkinter.Button(kb,text= button,width=6, bg="#3c4987", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#3c4987", relief='raised', padx=1,
+				pady=1, bd=1,command=command).grid(row=varRow,column=varColumn)
+
+		else:
+			tkinter.Button(kb,text= button,width=4, bg="#3c4987", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#3c4987", relief='raised', padx=1,
+				pady=1, bd=1,command=command).grid(row=varRow,column=varColumn)
+
+
+		varColumn +=1 
+
+		if varColumn > 14 and varRow == 2:
+			varColumn = 0
+			varRow+=1
+		if varColumn > 14 and varRow ==3:
+			varColumn = 0
+			varRow+=1
+
+def main():
+
 	
-	if button == "SPACE" or button == "SHIFT" or button == "BACK":
-		tkinter.Button(kb,text= button,width=6, bg="#000000", fg="#ffffff",
-			activebackground = "#ffffff", activeforeground="#000000", relief='raised', padx=1,
-			pady=1, bd=1,command=command).grid(row=varRow,column=varColumn)
+	kb.title("HosoKeys")
+	kb.resizable(0,0)
+	
 
-	else:
-		tkinter.Button(kb,text= button,width=4, bg="#000000", fg="#ffffff",
-			activebackground = "#ffffff", activeforeground="#000000", relief='raised', padx=1,
-			pady=1, bd=1,command=command).grid(row=varRow,column=varColumn)
+	label1 = Label(kb,text='               ').grid(row=0,columnspan=15)
 
+	global entry
+	entry = Entry(kb,width=50)
+	entry.grid(row=1,columnspan=15)
+	# entry.pack()
 
-	varColumn +=1 
+	entry.bind("<Button-1>", lambda e: HosoPop())
 
-	if varColumn > 14 and varRow == 2:
-		varColumn = 0
-		varRow+=1
-	if varColumn > 14 and varRow ==3:
-		varColumn = 0
-		varRow+=1
+	kb.mainloop()
 
-kb.mainloop()
+main()
